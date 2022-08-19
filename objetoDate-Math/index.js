66753a5f0c35f9e43800e31f59cad5766e69b2b9
Mathias Barbosa <76662048+mathiasbarbosa@users.noreply.github.com>
2022-08-19 20:17:08 -0300

    clase5 - math - date

---
+++ objetoDate-Math/index.js
@@ -0,0 +1,68 @@
+// console.log("Hola buenas tardes");
+
+// let fecha = new Date()
+
+// console.dir(fecha);
+
+
+
+// console.log(fecha.getDay());
+// console.log(fecha.getMonth());
+// console.log(fecha.getHours());
+
+// let fecha = new Date("2009-12-25")
+
+// let fechaActual = new Date()
+
+
+// console.log(fechaActual.getFullYear());
+// console.log(fechaActual);
+// console.log(fecha);
+
+// const milisegundosPorDia = 86400000
+
+// console.log( (fechaActual - fecha ) / milisegundosPorDia) // 8
+
+// let resultado = fechaActual - fecha
+
+// console.log(resultado);
+
+
+// const arrayDias = ["domingo","lunes", "martes","miercoles", "jueves", "viernes", "sabado"]
+
+// let fecha = new Date("2021-6-17")
+
+// console.dir(fecha);
+
+
+
+// // console.log(dia);
+
+// console.log(fecha.getDay())
+// console.log(fecha.getMonth());
+
+// fecha.setMeses = () => {
+//     console.log("Estoy acomodando los meses");
+// }
+
+// let numero = 0
+
+// let arrayNumeros = []
+
+// // let math = Math.PI
+
+// console.dir(Math.max(2, 4, 6, 7, 8, 3, 1))
+
+
+// let numero = (Math.random() * 100).toFixed(0)
+
+// console.log(numero);
+
+
+// let btn = document.querySelector("#token")
+// let numero;
+// btn.addEventListener("click", ( ) => {
+//    numero = (Math.random() * 100).toFixed(0)
+
+//     alert("tu token de seguridad es " + numero)
+// })
